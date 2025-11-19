898b9c2cbc0c2952afb11402f83ae91e12dcca59
josephclydemar <clydemar234@gmail.com>
2025-11-19 20:11:50 +0800

    Change the registers being used to comply to the linux ABI

--- src/raise_to_power.s
+++ src/raise_to_power.s
@@ -5,11 +5,11 @@ global raise_to_power
 
 raise_to_power:
   mov rax, 0x1
-  mov rcx, rsi
+  mov r8, rsi
 loop:
   imul rax, rdi
-  sub rcx, 0x1
-  cmp rcx, 0x0
+  sub r8, 0x1
+  cmp r8, 0x0
   jg loop
   ret
 
